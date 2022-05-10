8859712468c5aed11bea617e6cb48c350ae3adff
HairNee-HorLhar <eniseyinibrahim@gmail.com>
2022-05-10 05:53:12 +0100

    Advance task-(101-mul.c)

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -38,7 +38,7 @@ char *_memset(char *s, char b, unsigned int n)
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
   char *ptr;
-
+  
   if (nmemb == 0 || size == 0)
     return (NULL);
   ptr = malloc(nmemb * size);
