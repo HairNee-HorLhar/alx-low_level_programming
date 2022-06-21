ea697b802814493e7d910bab1a48e094fd34d423
ENISEYIN IBRAHIM <eniseyinibrahim@gmail.com>
2022-06-21 13:40:49 -0700

    Task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,29 +5,27 @@
  * needle in the string haystack.
  * @haystack: entire string.
  * @needle: substring.
- * 
  * Return: pointer to the beginning of located substring or
  * NULL if the substring is not found.
- */
-
+*/
 char *_strstr(char *haystack, char *needle)
 {
-	char *x_haystack;
-	char *x_needle;
-	
-	while (*haystack != '\0')
-	{
-		x_haystack = haystack;
-		x_needle = needle;
-		
-		while (*haystack != '\0' && *x_needle != '\0' && *haystack == *x_needle)
-		{
-			haystack++;
-			x_needle++;
-		}
-		if (!*x_needle)
-			return (x_haystack);
-		haystack = x_haystack + 1;
-	}
-	return (0);
+char *bhaystack;
+char *pneedle;
+
+while (*haystack != '\0')
+{
+bhaystack = haystack;
+pneedle = needle;
+
+while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
+{
+haystack++;
+pneedle++;
+}
+if (!*pneedle)
+return (bhaystack);
+haystack = bhaystack + 1;
+}
+return (0);
 }
